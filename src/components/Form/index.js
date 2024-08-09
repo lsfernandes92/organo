7dca21427fcf5cd7183dc8dc93e1c39e76740be3
lsfernandes92 <lsfernandes92@gmail.com>
2024-08-09 20:20:28 -0300

    Add form select component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,26 @@
 import "./Form.css"
 import InputText from '../InputText';
+import Dropdown from "../Dropdown";
 
 const Form = () => {
+  const teamList = [
+    'Programmers',
+    'Front-end',
+    'Data Science',
+    'Devops',
+    'UX & Design',
+    'Mobile',
+    'Innovation and management',
+  ]
+
   return (
     <section className="form">
       <form>
         <h2>Please fill in the following fields to create a card</h2>
         <InputText label="Name" placeholder="Type your name here" />
         <InputText label="Role" placeholder="Type your position here" />
-        <InputText label="Imagem" placeholder="Type here your image address" />
+        <InputText label="Image" placeholder="Type here your image address" />
+        <Dropdown label="Team" list={teamList} />
       </form>
     </section>
   );
