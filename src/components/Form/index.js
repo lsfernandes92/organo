2b6c48bd8789f655457cbdaf8a441dd10480d363
lsfernandes92 <lsfernandes92@gmail.com>
2024-08-10 20:56:34 -0300

    Finish form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,8 @@
 import "./Form.css"
 import InputText from '../InputText';
 import Dropdown from "../Dropdown";
+import Button from "../Button";
+import { useState } from "react";
 
 const Form = () => {
   const teamList = [
@@ -13,14 +15,50 @@ const Form = () => {
     'Innovation and management',
   ]
 
+  const [name, setName] = useState('')
+  const [role, setRole] = useState('')
+  const [image, setImage] = useState('')
+  const [team, setTeam] = useState('')
+
+  const onSave = (event) => {
+    event.preventDefault();
+    console.log("Form submitted with: ", name, role, image, team);
+  }
+
   return (
     <section className="form">
-      <form>
+      <form onSubmit={onSave}>
         <h2>Please fill in the following fields to create a card</h2>
-        <InputText label="Name" placeholder="Type your name here" />
-        <InputText label="Role" placeholder="Type your position here" />
-        <InputText label="Image" placeholder="Type here your image address" />
-        <Dropdown label="Team" list={teamList} />
+        <InputText 
+          label="Name"
+          required={true}
+          placeholder="Type your name here"
+          value={name}
+          handlerOnChange={value => setName(value)}
+        />
+        <InputText 
+          label="Role"
+          required={true}
+          placeholder="Type your position here"
+          value={role}
+          handlerOnChange={value => setRole(value)}
+        />
+        <InputText
+          label="Image"
+          placeholder="Type here your image address"
+          value={image}
+          handlerOnChange={value => setImage(value)}
+        />
+        <Dropdown 
+          label="Team"
+          list={teamList}
+          required={true}
+          value={team}
+          handlerOnChange={value => setTeam(value)}
+        />
+        <Button>
+          Create card
+        </Button>
       </form>
     </section>
   );
--- src/components/InputText/index.js
+++ src/components/InputText/index.js
@@ -3,10 +3,19 @@ import "./InputText.css"
 const InputText = (props) => {
   const placeHolderWithReticences = `${props.placeholder}...`
 
+  const handlerOnChange = (event) => {
+    props.handlerOnChange(event.target.value)
+  }
+
   return(
     <div className="input-text">
       <label>{props.label}</label>
-      <input placeholder={placeHolderWithReticences} />
+      <input 
+        value={props.value}
+        required={props.required}
+        placeholder={placeHolderWithReticences}
+        onChange={handlerOnChange}
+      />
     </div>
   );
 }
