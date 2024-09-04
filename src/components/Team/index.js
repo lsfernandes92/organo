bf3ab45e9e169f2a4d4235d80e613961219f16e6
lsfernandes92 <lsfernandes92@gmail.com>
2024-09-04 17:25:16 -0300

    Adjust UserCard header bg color

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -2,13 +2,18 @@ import UserCard from "../UserCard";
 import "./Team.css"
 
 export const Team = (props) => {
+  const primaryColor = props.primaryColor;
+  const secondaryColor = props.secondaryColor;
+
   return(
     (props.users.length > 0) &&
-    <section className="team" style={{"--team-bg-color": props.secondaryColor }}>
-      <h3 style={{"--team-border-color": props.primaryColor}}>{props.name}</h3>
+    <section className="team" style={{"--team-bg-color": secondaryColor }}>
+      <h3 style={{"--team-border-color": primaryColor}}>{props.name}</h3>
       <div className="users">
         {props.users.map(user => 
           <UserCard
+            key={user.name}
+            bgColor={primaryColor}
             image={user.image}
             name={user.name}
             role={user.role}
--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,9 +1,9 @@
 import "./UserCard.css"
 
-export const UserCard = ({ image, name, role }) => {
+export const UserCard = ({ image, name, role, bgColor }) => {
   return(
     <div className="person">
-      <div className="person-header">
+      <div className="person-header" style={{ "--person-header-bg-color": bgColor }}>
         <img src={image} alt={name}></img>
       </div>
       <div className="person-footer">
