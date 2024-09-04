0839f00054592ca1e61d3fb87c301b1b0be59df6
lsfernandes92 <lsfernandes92@gmail.com>
2024-09-04 17:12:27 -0300

    Add Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return(
+    <footer className="footer">
+      <div class="footer-socials">
+        <img src="/images/fb.png" alt="Facebook's icon"></img>
+        <img src="/images/tw.png" alt="Twitter's icon"></img>
+        <img src="/images/ig.png" alt="Instagram's icon"></img>
+      </div>
+      <div class="footer-logo">
+        <img src="/images/logo.png" alt="Logo from the Organo application"></img>
+      </div>
+      <p>Desenvolvido por Alura</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -3,12 +3,12 @@ import "./UserCard.css"
 export const UserCard = ({ image, name, role }) => {
   return(
     <div className="person">
-      <div className="header">
+      <div className="person-header">
         <img src={image} alt={name}></img>
       </div>
-      <div className="footer">
+      <div className="person-footer">
         <h4>{name}</h4>
-        <h3>{role}</h3>
+        <h5>{role}</h5>
       </div>
     </div>
   );
