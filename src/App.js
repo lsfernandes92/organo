6dfa03ddf0c58ff3274e2b3a993cbba0fe3500d9
lsfernandes92 <lsfernandes92@gmail.com>
2024-09-16 19:49:36 -0300

    Add feature to change team bg color

--- src/App.js
+++ src/App.js
@@ -3,45 +3,46 @@ import Banner from "./components/Banner";
 import Form from "./components/Form";
 import Team from "./components/Team";
 import Footer from "./components/Footer";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const teamList = [
+  const [teamList, setTeamList] = useState([
     {
+      id: uuidv4(),
       name: "Programmers",
-      primaryColor: "#57C278",
-      secondaryColor: "#D9F7E9",
+      color: "#57C278",
     },
     {
+      id: uuidv4(),
       name: "Front-end",
-      primaryColor: "#82CFFA",
-      secondaryColor: "#E8F8FF",
+      color: "#82CFFA",
     },
     {
+      id: uuidv4(),
       name: "Data Science",
-      primaryColor: "#A6D157",
-      secondaryColor: "#F0F8E2",
+      color: "#A6D157",
     },
     {
+      id: uuidv4(),
       name: "Devops",
-      primaryColor: "#E06B69",
-      secondaryColor: "#FDE7E8",
+      color: "#E06B69",
     },
     {
+      id: uuidv4(),
       name: "UX & Design",
-      primaryColor: "#DB6EBF",
-      secondaryColor: "#FAE9F5",
+      color: "#DB6EBF",
     },
     {
+      id: uuidv4(),
       name: "Mobile",
-      primaryColor: "#FFBA05",
-      secondaryColor: "#FFF5D9",
+      color: "#FFBA05",
     },
     {
+      id: uuidv4(),
       name: "Innovation and management",
-      primaryColor: "#FF8A29",
-      secondaryColor: "#FFEEDF",
+      color: "#FF8A29",
     },
-  ]
+  ])
 
   const initialTeamList = [
     {
@@ -54,21 +55,31 @@ function App() {
 
   const [users, setUsers] = useState(initialTeamList)
 
-  const handleOnUserCreate = (user) => {
+  const handlerOnUserCreate = (user) => {
     setUsers([...users, user])
+  }
+
+  const handlerOnChangeColor = (color, id) => {
+    setTeamList(teamList.map(team => {
+      if(team.id === id) {
+        team.color = color;
+      }
+      return team;
+    }))
   }
 
   return (
     <div className="App">
       <Banner />
-      <Form teamNames={teamList.map(team => team.name)} onUserCreate={user => handleOnUserCreate(user)}/>
+      <Form teamNames={teamList.map(team => team.name)} onUserCreate={user => handlerOnUserCreate(user)}/>
       {teamList.map(team =>
         <Team 
           key={team.name}
+          id={team.id}
           name={team.name}
-          primaryColor={team.primaryColor}
-          secondaryColor={team.secondaryColor}
+          color={team.color}
           users={users.filter(user => user.team === team.name)}
+          onChangeColor={handlerOnChangeColor}
         />
       )}
       <Footer />
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,9 +1,10 @@
 import UserCard from "../UserCard";
+import hexToRgba from 'hex-to-rgba';
 import "./Team.css"
 
 export const Team = (props) => {
-  const primaryColor = props.primaryColor;
-  const secondaryColor = props.secondaryColor;
+  const primaryColor = props.color;
+  const secondaryColor = hexToRgba(primaryColor, '0.5'); ;
   const bgImage = "url(/images/bg.png)";
 
   const handlerOnDelete = () => {
@@ -13,6 +14,12 @@ export const Team = (props) => {
   return(
     (props.users.length > 0) &&
     <section className="team" style={{"--team-bg-color": secondaryColor, "backgroundImage": bgImage }}>
+      <input 
+        type="color"
+        onChange={event => props.onChangeColor(event.target.value, props.id)}
+        value={primaryColor}
+        className="input-color"
+      />
       <h3 style={{"--team-border-color": primaryColor}}>{props.name}</h3>
       <div className="users">
         {props.users.map(user => {
